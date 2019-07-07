76bcbd877727166e958f13b5fc07b177687584c5
Samuel Parchert <parchertsam@gmail.com>
2019-07-07 23:40:55 +1000

    Added player class and cleaned up functions

--- api_requests.py
+++ api_requests.py
@@ -3,32 +3,59 @@ from session_init import session
 url_base = 'https://api.ausmash.com.au/'
 
 
+class IncorrectArgumentsError(Exception):
+    pass
+
+
 class Character(object):
     def __init__(self, id):
         self.id = id
+        self.name = ''
+        self.game_id = -1
+        self.colour = ''
+        self.icon_url = ''
+        self.css_url = ''
+        self.match_count = -1
+        self.result_count = -1
+        self.player_count = -1
+        self.update()
 
     @staticmethod
-    def show_all():
-        path = url_base + 'characters'
+    def get_characters(game=None):
+        if game is None:
+            path = url_base + 'characters'
+        else:
+            path = url_base + 'characters/bygame/{}'.format(game.id)
         response = session.get(path)
         return response.json()
 
     @staticmethod
-    def by_game(game):
-        path = url_base + 'characters/bygame/{}'.format(game.id)
-        response = session.get(path)
-        return response.json()
+    def get_character(name=None, game=None, id=None):
+        if id is None and name is not None and game is not None:
+            for result in Character.get_characters(game):
+                if name == result['Name']:
+                    return result
+        elif id is not None and name is None and Game is None:
+            path = url_base + 'characters/bygame/{}'.format(id)
+            response = session.get(path)
+            return response.json()
+        else:
+            raise IncorrectArgumentsError(
+                "Character.get_character should only be executed with "
+                "a name and game or a lone id"
+            )
 
-    @staticmethod
-    def get_id_from_name(name, game_acronym):
-        for result in Character.show_all():
-            if name == result['Name'] and game_acronym == result['GameShort']:
-                return result['ID']
-
-    def info(self):
+    def update(self):
         path = url_base + 'characters/{}'.format(self.id)
-        response = session.get(path)
-        return response.json()
+        response = session.get(path).json()
+        self.name = response['Name']
+        self.game_id = response['GameID']
+        self.colour = response['Colour']
+        self.icon_url = response['IconUrl']
+        self.css_url = response['CssUrl']
+        self.match_count = response['MatchCount']
+        self.result_count = response['ResultCount']
+        self.player_count = response['PlayerCount']
 
     def results(self):
         path = url_base + 'characters/{}/results'.format(self.id)
@@ -64,20 +91,137 @@ class Character(object):
 class Game(object):
     def __init__(self, id):
         self.id = id
+        self.name = ''
+        self.short = ''
+        self.update()
 
     @staticmethod
-    def show_all():
+    def get_games():
         path = url_base + 'games'
         response = session.get(path)
         return response.json()
 
     @staticmethod
-    def get_id_from_acronym(acronym):
-        for result in Game.show_all():
-            if acronym == result['Short']:
-                return result['ID']
+    def get_game(id=None, short=None, name=None):
+        if sum(x is not None for x in (id, short, name)) != 1:
+            raise IncorrectArgumentsError(
+                "Game.get_game should only be executed with "
+                "one argument."
+            )
+        elif id is not None:
+            path = url_base + 'games/{}'.format(id)
+            response = session.get(path)
+            return response.json()
+        elif short is not None:
+            for result in Game.get_games():
+                if short == result['Short']:
+                    return result.json()
+        elif name is not None:
+            for result in Game.get_games():
+                if name == result['Name']:
+                    return result.json()
 
-    def info(self):
+    def update(self):
         path = url_base + 'games/{}'.format(self.id)
+        response = session.get(path).json()
+        self.name = response['Name']
+        self.short = response['Short']
+
+
+class Player(object):
+    def __init__(self, id):
+        self.id = id
+        self.name = ''
+        self.region_short = ''
+        self.region_id = ''
+        self.personal_url = ''
+        self.twitch_url = ''
+        self.youtube_url = ''
+        self.twitter_url = ''
+        self.facebook_url = ''
+        self.ssb_world_url = ''
+        self.bio = ''
+        self.tournament_count = -1
+        self.match_count = -1
+        self.result_count = -1
+        self.video_count = -1
+        self.update()
+
+    @staticmethod
+    def get_players(region=None):
+        if region is None:
+            path = url_base + 'players'
+        else:
+            path = url_base + 'players/byregion/{}'.format(region)
+        response = session.get(path)
+        return response.json()
+
+    @staticmethod
+    def get_player(name=None, region=None, id=None):
+        if id is None and name is not None and region is not None:
+            path = url_base + 'players/find/{}/{}'.format(name, region)
+        elif id is not None and name is None and region is None:
+            path = url_base + 'players/{}'.format(id)
+        else:
+            raise IncorrectArgumentsError(
+                "Player.get_player should only be executed with "
+                "a name and region or a lone id"
+            )
+        response = session.get(path)
+        return response.json()
+
+    def update(self):
+        path = url_base + 'players/{}'.format(self.id)
+        response = session.get(path).json()
+        self.name = response['Name']
+        self.region_short = response['RegionShort']
+        self.region_id = response['RegionID']
+        self.personal_url = response['PersonalUrl']
+        self.twitch_url = response['TwitchUrl']
+        self.youtube_url = response['YouTubeUrl']
+        self.twitter_url = response['TwitterUrl']
+        self.facebook_url = response['FacebookUrl']
+        self.ssb_world_url = response['SSBWorldUrl']
+        self.bio = response['Bio']
+        self.tournament_count = response['TournamentCount']
+        self.match_count = response['MatchCount']
+        self.result_count = response['ResultCount']
+        self.video_count = response['VideoCount']
+
+    def get_elo(self):
+        path = url_base + 'players/{}/elo'.format(self.id)
+        response = session.get(path)
+        return response.json()
+
+    def get_trueskill(self):
+        path = url_base + 'players/{}/trueskill'.format(self.id)
+        response = session.get(path)
+        return response.json()
+
+    def get_results(self):
+        path = url_base + 'players/{}/results'.format(self.id)
+        response = session.get(path)
+        return response.json()
+
+    def get_matches(self):
+        path = url_base + 'players/{}/matches'.format(self.id)
+        response = session.get(path)
+        return response.json()
+
+    def get_rankings(self):
+        path = url_base + 'players/{}/rankings'.format(self.id)
+        response = session.get(path)
+        return response.json()
+
+    def get_videos(self, character=None):
+        if character is not None:
+            path = url_base + 'players/{}/videos/{}'.format(self.id, character.id)
+        else:
+            path = url_base + 'players/{}/videos'.format(self.id)
+        response = session.get(path)
+        return response.json()
+
+    def get_winrates(self, game):
+        path = url_base + 'players/{}/winrates/{}'.format(self.id, game.id)
         response = session.get(path)
         return response.json()
